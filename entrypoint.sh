69abe25f601abb3ee2a73196ce80a78b277717cf
Brian Sizemore <bpsizemore@gmail.com>
2024-02-18 16:47:06 -0500

    updating action

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,30 @@ echo "noseyparker-action startup..."
 time=$(date)
 echo "Current time: $time"
 
-echo "The first 3 args are..."
-echo $1
-echo $2
-echo $3
+    - ${{ inputs.upload-reports }}
+    - ${{ inputs.report-format-human }}
+    - ${{ inputs.report-format-json }}
+    - ${{ inputs.report-format-jsonl }}
+    - ${{ inputs.report-format-sarif }}
+    - ${{ inputs.github-username }}
 
-echo "Starting scan for ..."
-noseyparker scan --datastore np.action --github-user $1
+NP_UPLOAD_REPORTS=$1
+NP_REPORT_FORMAT_HUMAN=$2
+NP_REPORT_FORMAT_JSON=$3
+NP_REPORT_FORMAT_JSONL=$4
+NP_REPORT_FORMAT_SARIF=$5
+NP_GITHUB_USERNAME=$6
+
+echo "Configuration: "
+echo "UPLOAD_REPORTS: $NP_UPLOAD_REPORTS"
+echo "REPORT_FORMAT_HUMAN: $NP_REPORT_FORMAT_HUMAN"
+echo "REPORT_FORMAT_JSON: $NP_REPORT_FORMAT_JSON"
+echo "REPORT_FORMAT_JSONL: $NP_REPORT_FORMAT_JSONL"
+echo "REPORT_FORMAT_SARIF: $NP_REPORT_FORMAT_SARIF"
+echo "GITHUB_USERNAME: $NP_GITHUB_USERNAME"
+
+echo "Starting scan for $NP_GITHUB_USERNAME"
+noseyparker scan --datastore np.action --github-user $NP_GITHUB_USERNAME
 NP_STATUS_CODE=$?
 
 echo "Scan complete. Status code: $NP_STATUS_CODE"
@@ -19,18 +36,39 @@ if [ $NP_STATUS_CODE -eq 0 ]
 then
     NP_STATUS="success"
 else
+	# Scan failed - exit early
     NP_STATUS="failed"
+    exit 1
 fi
 
-echo "Fetching summary and report..."
+
+
+if [[ $NP_UPLOAD_REPORTS == "true" ]]; then
+   if [[ $NP_REPORT_FORMAT_HUMAN == "true" ]]; then
+      noseyparker report --datastore=np.action --format=human --output=/github/workspace/report.human
+   fi
+   
+   if [[ $NP_REPORT_FORMAT_JSON == "true" ]]; then
+      noseyparker report --datastore=np.action --format=json --output=/github/workspace/report.json
+   fi
+   
+   if [[ $NP_REPORT_FORMAT_JSONL == "true" ]]; then
+      noseyparker report --datastore=np.action --format=jsonl --output=/github/workspace/report.jsonl
+   fi
+   
+   if [[ $NP_REPORT_FORMAT_SARIF == "true" ]]; then
+      noseyparker report --datastore=np.action --format=sarif --output=/github/workspace/report.sarif
+   fi
+else
+	echo "Skipping reports. Set upload-reports to 'true' in your actions file to run."
+fi
 
 NP_SUMMARY=$(noseyparker summarize --datastore np.action)
-NP_REPORT=$(noseyparker report --datastore np.action)
+echo "Report summary..."
+echo $NP_SUMMARY
 
-echo "Complete, exporting data."
+echo "Action Complete - exporting data."
 
 echo "np_status_code=$NP_STATUS_CODE" >> $GITHUB_OUTPUT
 echo "np_status=$NP_STATUS" >> $GITHUB_OUTPUT
-echo "np_summary=$NP_SUMMARY" >> $GITHUB_OUTPUT
-echo "np_report=$NP_REPORT" >> $GITHUB_OUTPUT
 echo "time=$time" >> $GITHUB_OUTPUT
